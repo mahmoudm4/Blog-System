91e81c6b483a0bdf988df50bbdd94c4b320eae26
mahmoudm4 <mahmoudm4895@gmail.com>
2021-03-20 15:39:56 +0200

    add showPost.blade.php

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,12 +8,21 @@ class PostController extends Controller
 {
     public function index(){
         $allposts = [
-            ['id' => 1, 'title' => 'Laravel', 'posted_by' => 'Ali', 'created_at' => '2021-03-04'],
-            ['id' => 2, 'title' => 'Java', 'posted_by' => 'Ahmed', 'created_at' => '2021-10-24'],
-            ['id' => 3, 'title' => 'C#', 'posted_by' => 'Mahmoud', 'created_at' => '2021-08-14']
+            ['id' => 1, 'title' => 'Laravel', 'posted_by' => 'Ali', 'created_at' => '2021-03-04','creator'=>['name'=>'Ali','email'=>'ali@ali.com']],
+            ['id' => 2, 'title' => 'Java', 'posted_by' => 'Ahmed', 'created_at' => '2021-10-24'],'creator'=>['name'=>'Ahmed','email'=>'ali@ali.com'],
+            ['id' => 3, 'title' => 'C#', 'posted_by' => 'Mahmoud', 'created_at' => '2021-08-14','creator'=>['name'=>'Mahmoud','email'=>'ali@ali.com']]
         ];
         return view('posts.index',[
             'posts'=> $allposts
+        ]);
+    }
+
+    public function show($postid)
+    {
+        $post = ['id' => 1, 'title' => 'Laravel','desc'=>'Laravel is an awesome framework' ,'posted_by' => 'Ali', 'created_at' => '2021-03-04','creator'=>['name'=>'Ali','email'=>'ali@ali.com']];
+
+        return view('posts.show',[
+            'post' =>$post
         ]);
     }
 }
---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <span class="navbar-brand mb-0 h1">Navbar</span>
+    </nav>
+    <div class="container p-3">
+        <div class="card">
+            <div class="card-header">
+            Post Info
+            </div>
+            <div class="card-body">
+            <h5 class="card-title">Title</h5>
+            <p class="card-text">{{$post['title']}}</p>
+            <h5 class="card-title">Description</h5>
+            <p class="card-text">{{$post['desc']}}</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+            Post Creator Info
+            </div>
+            <div class="card-body">
+            <div>
+                <h5 class="card-title d-inline">Name: </h5>
+                <p class="card-text d-inline">{{$post['creator']['name']}}</p>
+            </div>
+            <div>
+                <h5 class="card-title d-inline">Email: </h5>
+                <p class="card-text d-inline">{{$post['creator']['email']}}</p>
+            </div>
+            <div>
+                <h5 class="card-title d-inline">Title: </h5>
+                <p class="card-text d-inline">{{$post['title']}}</p>
+            </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+</body>
+</html>
