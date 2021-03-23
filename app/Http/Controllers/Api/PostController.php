c970082c96fe5eeb380d49ea380f405451127057
mahmoudm4 <mahmoudm4895@gmail.com>
2021-03-23 14:35:05 +0200

    add Store Method

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -18,5 +19,11 @@ class PostController extends Controller
         
         return new PostResource($post);
     }
-    
+    public function store(StorePostRequest $request){
+
+        //dd('we are in store');
+        $post = Post::create($request->all());
+        return new PostResource($post);
+   
+    }
 }
--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -16,7 +16,7 @@ class UserResource extends JsonResource
     {
         return [
             'id'=>$this->id,
-            'name'=> $this->user,
+            'name'=> $this->name,
         ];
     }
 }
